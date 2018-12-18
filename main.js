d6762e2fdee1e5280d2891908a30e9afd0ec1ad7
Matthew Dean <matthew-dean@users.noreply.github.com>
2018-12-17 22:12:53 -0800

    some methods

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // ESM syntax is supported.
-import {get, set, has} from 'lodash'
+import {set as lset, has, merge} from 'lodash'
 
 class Node {
   constructor(ident, map) {
@@ -8,11 +8,22 @@ class Node {
   }
 }
 
+const set = (obj, path, value) => {
+  const val = get(obj, path)
+  if (val !== undefined) {
+    lset(obj, path, val.concat(value))
+  } else {
+    lset(obj, path, [value])
+  }
+}
+
 class StyleScope {
   constructor() {
     this.refs = {}
     this.root = this
     this.tree = []
+    this.functions = {}
+    this.path = []
   }
 
   set(path, value, root) {
@@ -20,44 +31,62 @@ class StyleScope {
       set(this.root, ['refs'].concat(path), value)
     } else {
       set(this.refs, path, value)
+      set(this.root, ['refs'].concat(this.path).concat(path), value)
     }
   }
 
-  get(path) {
+  _get(path) {
     if (has(this.refs, path)) {
       return get(this.refs, path)
     }
     return get(this.parent, ['refs'].concat(path))
   }
 
-  config(obj) {
-    set(this.config)
+  get(path) {
+    return this._get(path)
   }
 
-  addScope($) {
+  config(obj) {
+    merge(this.config, obj)
+  }
+
+  addScope($, path) {
     return new class extends StyleScope {
       constructor() {
         super()
         this.parent = $
+        this.path = path
         const parentConfig = get(this.parent, 'config')
         this.config = {...parentConfig}
       }
     }
   }
 
-  normalize(node) {
-    if (typeof node === 'string') {
-
-    }
-  }
-
-  addRules(node, func) {
+  rule(node, func) {
     const scope = addScope(this)
-    const n = this.normalize(node)
     this.tree.push([node, func.bind(scope)])
   }
 
-  addRule(node, value) {
-    this.tree.push([node, value])
+  decl(node) {
+    this.tree.push(node)
+  }
+
+  function(path, args, func, condition) {
+    const scope = addScope(this, path)
+    this.set(path, [func, scope, args, condition])
+  }
+
+  /**
+   * $.call('.mixin', [['val', 3]])
+   * $.call(['#ns', '.mixin'], [3])
+   *
+   * @param {*} path 
+   */
+  call(path, args) {
+    const val = this._get(path)
+    val.forEach(funcDef => {
+      const $ = funcDef[1]
+      $.set(path)
+    })
   }
 }
