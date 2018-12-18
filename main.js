99f6898df7a1f1ecd492aafbe7e54c9fee1020bf
Matthew Dean <matthew-dean@users.noreply.github.com>
2018-12-18 11:13:19 -0800

    Basic test added

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // ESM syntax is supported.
-import {set as lset, has, merge} from 'lodash'
+import {set as lset, get, has, merge} from 'lodash'
 
 class Node {
   constructor(ident, map) {
@@ -17,21 +17,25 @@ const set = (obj, path, value) => {
   }
 }
 
-class StyleScope {
+export class StyleScope {
   constructor() {
     this.refs = {}
     this.root = this
     this.tree = []
-    this.functions = {}
     this.path = []
   }
 
   set(path, value, root) {
+    if(typeof path === 'string') {
+      path = [path]
+    }
     if (root) {
       set(this.root, ['refs'].concat(path), value)
     } else {
       set(this.refs, path, value)
-      set(this.root, ['refs'].concat(this.path).concat(path), value)
+      if (this.parent) {
+        set(this.root, ['refs'].concat(this.path).concat(path), value)
+      }
     }
   }
 
@@ -56,23 +60,24 @@ class StyleScope {
         super()
         this.parent = $
         this.path = path
+        this.tree = []
         const parentConfig = get(this.parent, 'config')
         this.config = {...parentConfig}
       }
     }
   }
 
-  rule(node, func) {
-    const scope = addScope(this)
+  rules(node, func) {
+    const scope = this.addScope(this)
     this.tree.push([node, func.bind(scope)])
   }
 
-  decl(node) {
+  declaration(node) {
     this.tree.push(node)
   }
 
   function(path, args, func, condition) {
-    const scope = addScope(this, path)
+    const scope = this.addScope(this, [path])
     this.set(path, [func, scope, args, condition])
   }
 
@@ -84,9 +89,19 @@ class StyleScope {
    */
   call(path, args) {
     const val = this._get(path)
+    console.log(val)
     val.forEach(funcDef => {
       const $ = funcDef[1]
       $.set(path)
+    })
+  }
+
+  eval(tree = this.tree) {
+    tree.forEach(node => {
+      if (typeof node[1] === 'function') {
+        node[1]()
+        // todo
+      }
     })
   }
 }
---
+++ test/index.js
@@ -0,0 +1,28 @@
+let jessCLR = require('..')
+
+const styles = config => {
+  let {$, ...rest} = config
+  // variables are stripped of $ or @
+  // $.set calls are hoisted to top of block for Less transpilation
+  $.set('blah', '#555');
+
+  // will register as '.some-mixin` and 'some-mixin' as fallback
+  $.function('.some-mixin', ['val'], $ => {
+    $.declaration('bar', $.e($ => $.get('val')))
+  })
+
+  // Rules will be added as function call for Less
+  $.rules('.box', $ => {
+    $.declaration('color', $.e($ => $.get('blah')))
+    $.call('.some-mixin', {['val']: 'bar'})
+    $.rules('.sub', $ => {
+      $.declaration('foo', 'bar')
+    })
+  })
+
+  return {$, ...rest}
+}
+
+let $ = new jessCLR.StyleScope()
+
+console.log(styles({$}))
